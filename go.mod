a4925231c8d475612f10c578f09e44fff5715a23
fumiama <s862105088@gmail.com>
2021-09-17 15:19:50 +0800

    a

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/IPoWS/NCL
 
 go 1.16
 
-require github.com/IPoWS/node-core v0.0.0-20210916163404-65036459735f
+require (
+	github.com/IPoWS/node-core v0.0.0-20210917063800-de881974ed78
+	github.com/sirupsen/logrus v1.8.1
+)
--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/IPoWS/node-core/ip64"
 	"github.com/IPoWS/node-core/link"
 	"github.com/IPoWS/node-core/upper"
+	"github.com/sirupsen/logrus"
 )
 
 type txtservice struct {
@@ -17,17 +19,37 @@ func (s *txtservice) Handle(srcport uint16, destport uint16, data *[]byte) {
 }
 
 func main() {
+	logrus.SetLevel(logrus.DebugLevel)
+	logrus.Debugln("[NCL] start.")
 	link.InitEntry("ws://"+os.Args[1]+"/nps", os.Args[2], os.Args[3], 0xffff_ffff_0000_0000)
-	link.Register()
-	ts := upper.Service(new(txtservice))
-	upper.Register(1, &ts)
-	for {
-		var i uint64
-		var s []byte
-		fmt.Print("Enter ip:")
-		fmt.Scanf("%x", &i)
-		fmt.Print("Enter msg:")
-		fmt.Scanln(&s)
-		link.Send(i, &s, ip64.DataType, 1, 1)
+	logrus.Debugln("[NCL] init ent succ.")
+	err := link.Register()
+	if err == nil {
+		logrus.Debugln("[NCL] reg succ.")
+		err = link.ListenAccess()
+		if err == nil {
+			logrus.Debugln("[NCL] listen succ.")
+			ts := upper.Service(new(txtservice))
+			upper.Register(1, &ts)
+			logrus.Debugln("[NCL] service registered.")
+			time.Sleep(time.Second)
+			logrus.Debugln("[NCL] enter loop.")
+			for {
+				var i uint64
+				s := make([]byte, 512)
+				fmt.Print("Enter ip:")
+				fmt.Scanf("%x", &i)
+				fmt.Print("Enter msg:")
+				fmt.Scanln(&s)
+				_, err = link.Send(i, &s, ip64.DataType, 1, 1)
+				if err != nil {
+					logrus.Errorln("[NCL] send err:", err)
+				} else {
+					logrus.Infof("[NCL] send to %x succ.", i)
+				}
+			}
+		} else {
+			logrus.Errorln("[NCL] listen access err:", err)
+		}
 	}
 }
